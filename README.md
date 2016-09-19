c974220ff70091836f6786171f41d73d0ea8553e
buddytherat <buddytherat@gmail.com>
2016-09-18 19:46:53 -0500

    update readme

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # tree-editor
+## ToDo
++ Nodes should be draggable.
+  + X and Y should be saved in editData.
++ Save to JSON file.
++ Load from JASON file.
--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ define(function(require) {
   var events = require('./pubsub.js');
   var Node = require('./node.js');
 
+  //cache DOM
   var nodes = [];
   var $c = document.getElementById('node-container');
   var $e = document.getElementById('editor-container');
@@ -14,21 +15,6 @@ define(function(require) {
     nodes.push(node);
 
     $c.appendChild(node.elem);
-  });
-
-  events.on('save', function(data) {
-    var editNode = nodes[data.editData.index];
-    editNode.gameData = data.gameData;
-    editNode.editData = data.editData;
-    console.log(nodes);
-  });
-
-  events.on('edit', function(data) {
-    $e.style.display = 'block';
-    $form.elements['index'].value = data.editData.index;
-    $form.elements['name'].value = data.gameData.name;
-    $form.elements['title'].value = data.gameData.title;
-    $form.elements['description'].value = data.gameData.description;
   });
 
   $form.addEventListener('submit', function save(e) {
@@ -46,6 +32,22 @@ define(function(require) {
     }
     events.emit('save', new_data);
   });
+
+  events.on('save', function(data) {
+    var editNode = nodes[data.editData.index];
+    editNode.gameData = data.gameData;
+    editNode.editData = data.editData;
+    console.log(nodes);
+  });
+
+  events.on('edit', function(data) {
+    $e.style.display = 'block';
+    $form.elements['index'].value = data.editData.index;
+    $form.elements['name'].value = data.gameData.name;
+    $form.elements['title'].value = data.gameData.title;
+    $form.elements['description'].value = data.gameData.description;
+  });
+
 });
 
 
